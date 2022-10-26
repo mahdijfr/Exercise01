aedf353b8b76547239080cbfe12f7f66946ac9c9
mahdijfr <mahdijafarinasr@gmail.com>
2022-10-26 18:09:34 +0330

    Comments corrected

--- Main.java
+++ Main.java
@@ -2,6 +2,15 @@ import java.util.*;
 
 public class Main {
 
+    /**
+     * @param id     is the contact id witch is saved in the contact object later on
+     * @param index  is used as index of contact array to access each object in this
+     *               array of objects from contact class
+     * @param con    is an array of objects from Contact class
+     * @param jline  is simply an input object
+     * @param status is used to declare if the code is running or not (switches by
+     *               the exit command from user)
+     */
     public static void main(String[] args) {
         Scanner jline = new Scanner(System.in);
         Contact con[] = new Contact[1000];
@@ -9,8 +18,18 @@ public class Main {
         int index = 0;
         int status = 0;
         while (status == 0) {
+            /* recieving the command */
             System.out.print("enter your command: ");
             String command = jline.nextLine();
+            /*
+             * checking what the command is
+             * an acting according to that
+             */
+
+            /*
+             * when add is called
+             * this assign the input values to objects
+             */
             if (command.equals("add")) {
                 con[index] = new Contact();
                 System.out.print("    first name: ");
@@ -29,11 +48,19 @@ public class Main {
                 id += 1;
                 index += 1;
             }
+            /*
+             * this sets the empty objects of array to null
+             * avoiding runtime errors
+             */
             for (int j = 0; j < con.length; j++) {
                 if (j > index) {
                     con[j] = new Contact(0);
                 }
             }
+            /*
+             * When list command is called
+             * Displays all saved commands so far.
+             */
             if (command.equals("list")) {
                 int i = 0;
                 while (i < index) {
@@ -41,6 +68,9 @@ public class Main {
                     i += 1;
                 }
             }
+            /*
+             * the following parts are for the search feature
+             */
             if (command.equals("search-by-phone")) {
                 int pfound = 0;
                 System.out.print("phone number: ");
@@ -55,6 +85,10 @@ public class Main {
                         k += 1;
                     }
                 }
+                /*
+                 * This part is neccessary to avoid outputting the not found message for
+                 * multiple times
+                 */
                 if (pfound == 0) {
                     System.out.println("####");
                     System.out.println("no contacts found");
